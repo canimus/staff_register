08b19590d66d08d5bed85f6742e703ee73bdddbf
Herminio Vazquez <canimus@gmail.com>
2015-11-10 23:55:27 +0800

    Adding comments

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -9,6 +9,7 @@ class Employee < ActiveRecord::Base
 		self.time_offs.count > 0
 	end
 
+	# array of dates with leave in current year
 	def leave(time_frame=Range.new(Chronic.parse('1/1', context: :past), Chronic.parse('12/31')))
 		self.time_offs.where start_at:time_frame
 	end
